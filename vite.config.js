eba0405ab734a293d078857d31e0c5b31f5ac810
manorajsivanmalai <smanoraj24@gmail.com>
2025-12-30 10:13:50 +0530

    design with static frontend version1

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude:["**/*.glb"],
-  server: {
-    allowedHosts: [
-      'reviewer-obtain-silence-versus.trycloudflare.com'
-    ]
-  }
 })
